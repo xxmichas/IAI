536c1da3a63b4fd1d257f579cdc8092d8aed13b2
Michal Orkisz <xxmichas@gmail.com>
2021-01-29 21:44:46 +0100

    post-swiper

--- src/containers/Product/Product.js
+++ src/containers/Product/Product.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import styles from './Product.module.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import Tippmann from '../../assets/products/Tippmann.png'
 
 const Product = () => {
+    let cards = [
+                <div className={styles.card}><img src={Tippmann} alt="Tippmann"></img></div>,
+                <div className={styles.card}><img src={Tippmann} alt="Tippmann"></img></div>,
+                <div className={styles.card}><img src={Tippmann} alt="Tippmann"></img></div>,
+                <div className={styles.card}><img src={Tippmann} alt="Tippmann"></img></div>,
+                <div className={styles.card}><img src={Tippmann} alt="Tippmann"></img></div>,
+                <div className={styles.card}><img src={Tippmann} alt="Tippmann"></img></div>,
+                <div className={styles.card}><img src={Tippmann} alt="Tippmann"></img></div>,
+                <div className={styles.card}><img src={Tippmann} alt="Tippmann"></img></div>,
+                <div className={styles.card}><img src={Tippmann} alt="Tippmann"></img></div>
+            ]
+
     return (
         <div className={styles.Product}>
-            dsadsa
+            <div className={styles.carousel}>
+                <Swiper spaceBetween={10} slidesPerView={6}  preventInteractionOnTransition direction="vertical">
+                    {cards.map((el, i) => (<SwiperSlide key={i}>{el}</SwiperSlide>))}
+                </Swiper>
+            </div>
         </div>
     )
 }
