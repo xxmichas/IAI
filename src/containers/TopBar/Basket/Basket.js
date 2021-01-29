332bf737ca2ff48ea1c042ac61fb91669f2f83b7
Michal Orkisz <xxmichas@gmail.com>
2021-01-29 13:19:17 +0100

    small fixes

--- src/containers/TopBar/Basket/Basket.js
+++ src/containers/TopBar/Basket/Basket.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from './Basket.module.css'
+import badge from '../../../assets/badge.png'
 
 const Basket = () => {
     return (
         <div className={styles.Basket}>
+            <img src={badge} alt="Koszyk" useMap="#Basket" />
+            <map name="Basket">
+                <area className={styles.BasketMap} alt="Koszyk" coords="0,0 0,131 92,155 185,131 185,0 " shape="polygon" />
+            </map>
         </div>
     )
 }
