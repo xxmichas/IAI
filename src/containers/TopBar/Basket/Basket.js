f72aed50af9fb0988c8a856962a399c699f9c879
Michal Orkisz <xxmichas@gmail.com>
2021-01-29 13:52:27 +0100

    basket

--- src/containers/TopBar/Basket/Basket.js
+++ src/containers/TopBar/Basket/Basket.js
@@ -5,10 +5,15 @@ import badge from '../../../assets/badge.png'
 const Basket = () => {
     return (
         <div className={styles.Basket}>
-            <img src={badge} alt="Koszyk" useMap="#Basket" />
-            <map name="Basket">
-                <area className={styles.BasketMap} alt="Koszyk" coords="0,0 0,131 92,155 185,131 185,0 " shape="polygon" />
-            </map>
+            <div className={styles.background}>
+                <img src={badge} alt="Koszyk" useMap="#Basket" />
+                <map name="Basket">
+                    <area className={styles.BasketMap} alt="Koszyk" coords="0,0 0,131 92,155 185,131 185,0 " shape="polygon" />
+                </map>
+            </div>
+            <div className={styles.content}>
+                <div className={styles.rank} />
+            </div>
         </div>
     )
 }
