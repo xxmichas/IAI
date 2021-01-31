797cdac229cdb100ce086e4d55f64bad066edbe4
Michal Orkisz <xxmichas@gmail.com>
2021-01-31 22:32:04 +0100

    suggested

--- src/containers/Suggested/Suggested.js
+++ src/containers/Suggested/Suggested.js
@@ -32,6 +32,40 @@ const Suggested = () => {
                         </div>
                     </div>
                 </div>
+                <div className={styles.item}>
+                    <div className={styles.itemImg}>
+                        <img src={item} alt="MASKA EMPIRE E-FLEX GOGGLE (BLACK)" />
+                    </div>
+                    <div className={styles.texts}>
+                        <div className={styles.text}>
+                            <div className={styles.name}>
+                                MASKA EMPIRE E-FLEX GOGGLE (BLACK)
+                            </div>
+                        </div>
+                        <div className={styles.text}>
+                            <div className={styles.price}>
+                                139,00 zł
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.item}>
+                    <div className={styles.itemImg}>
+                        <img src={item} alt="MASKA EMPIRE E-FLEX GOGGLE (BLACK)" />
+                    </div>
+                    <div className={styles.texts}>
+                        <div className={styles.text}>
+                            <div className={styles.name}>
+                                MASKA EMPIRE E-FLEX GOGGLE (BLACK)
+                            </div>
+                        </div>
+                        <div className={styles.text}>
+                            <div className={styles.price}>
+                                139,00 zł
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
