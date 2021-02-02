801f0494175a4b10b3cf4cbca46c5b1e0a8c8215
Michal Orkisz <xxmichas@gmail.com>
2021-02-02 19:48:53 +0100

    RWD - part 4

--- src/containers/Description/Description.js
+++ src/containers/Description/Description.js
@@ -81,47 +81,61 @@ const Description = () => {
                     </div>
                     <div className={styles.detailsContainer}>
                         <div className={styles.detailsText}>
-                            <div className={styles.detailsProp}>
-                                Numer seryjny:
+                            <div className={styles.detail}>
+                                <div className={styles.detailsProp}>
+                                    Numer seryjny:
+                                </div>
+                                <div className={styles.detailsValue}>
+                                    111000
+                                </div>
                             </div>
-                            <div className={styles.detailsValue}>
-                                111000
+                            <div className={styles.detail}>
+                                <div className={styles.detailsProp}>
+                                    Model:
+                                </div>
+                                <div className={styles.detailsValue}>
+                                    TIPPMANN FT- 50 LITE 50
+                                </div>
                             </div>
-                            <div className={styles.detailsProp}>
-                                Model:
+                            <div className={styles.detail}>
+                                <div className={styles.detailsProp}>
+                                    Kaliber:
+                                </div>
+                                <div className={styles.detailsValue}>
+                                    66 LR HV
+                                </div>
                             </div>
-                            <div className={styles.detailsValue}>
-                                TIPPMANN FT- 50 LITE 50
+                            <div className={styles.detail}>
+                                <div className={styles.detailsProp}>
+                                    Pojemność magazynka:
+                                </div>
+                                <div className={styles.detailsValue}>
+                                    10 nabojów
+                                </div>
                             </div>
-                            <div className={styles.detailsProp}>
-                                Kaliber:
+                            <div className={styles.detail}>
+                                <div className={styles.detailsProp}>
+                                    Mechanizm spustowy:
+                                </div>
+                                <div className={styles.detailsValue}>
+                                    SA (Single Action)
+                                </div>
                             </div>
-                            <div className={styles.detailsValue}>
-                                66 LR HV
+                            <div className={styles.detail}>
+                                <div className={styles.detailsProp}>
+                                    Siła nacisku spustu:
+                                </div>
+                                <div className={styles.detailsValue}>
+                                    1.8 kg / 4 lb
+                                </div>
                             </div>
-                            <div className={styles.detailsProp}>
-                                Pojemność magazynka:
-                            </div>
-                            <div className={styles.detailsValue}>
-                                10 nabojów
-                            </div>
-                            <div className={styles.detailsProp}>
-                                Mechanizm spustowy:
-                            </div>
-                            <div className={styles.detailsValue}>
-                                SA (Single Action)
-                            </div>
-                            <div className={styles.detailsProp}>
-                                Siła nacisku spustu:
-                            </div>
-                            <div className={styles.detailsValue}>
-                                1.8 kg / 4 lb
-                            </div>
-                            <div className={styles.detailsProp}>
-                                Wymiary:
-                            </div>
-                            <div className={styles.detailsValue}>
-                                1200x123mm
+                            <div className={styles.detail}>
+                                <div className={styles.detailsProp}>
+                                    Wymiary:
+                                </div>
+                                <div className={styles.detailsValue}>
+                                    1200x123mm
+                                </div>
                             </div>
                             <div className={styles.detailsPaint} />
                         </div>
