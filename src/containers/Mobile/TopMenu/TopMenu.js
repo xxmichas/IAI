8684c6110ae4c94280003170d7123d1cd3ebfa22
Michal Orkisz <xxmichas@gmail.com>
2021-02-02 21:44:36 +0100

    mobile - part 1

---
+++ src/containers/Mobile/TopMenu/TopMenu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './TopMenu.module.css'
+
+const TopMenu = () => {
+    return (
+        <div className={styles.TopMenu}>
+            <div className={styles.bars}>
+                
+            </div>
+        </div>
+    )
+}
+
+export default TopMenu
--- src/containers/TopBar/TopBar.js
+++ src/containers/TopBar/TopBar.js
@@ -1,16 +1,41 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import TopLinks from '../TopLinks/TopLinks'
 import styles from './TopBar.module.css'
 import logo from '../../assets/logo.png'
 import magnifyingGlass from '../../assets/magnifying-glass.png'
 import LanguageSelect from '../LanguageSelect/LanguageSelect'
 import Basket from './Basket/Basket'
+import TopMenu from '../Mobile/TopMenu/TopMenu'
 
 const TopBar = (props) => {
+    const mediaQuery = window.matchMedia('(max-width: 800px)')
+
+    let [IsMobile, SetIsMobile] = useState(false)
+
+    let handleResize = (e) => {
+        if (e.matches) {
+            console.log('Media Query Matched!')
+            SetIsMobile(true)
+        }
+        else {
+            SetIsMobile(false)
+        }
+    }
+
+    useEffect(() => {
+        mediaQuery.addEventListener("change", handleResize)
+        handleResize(mediaQuery)
+
+        return () => {
+            mediaQuery.removeEventListener("change", handleResize)
+        }
+        //eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
         <Fragment>
             <div className={styles.TopBar} />
-            <TopLinks />
+            {IsMobile ? <TopMenu /> : <TopLinks />}
             <div className={styles.content}>
                 <div className={styles.logo}>
                     <img onClick={() => window.location.assign(window.location.origin)} src={logo} alt="logo" />
