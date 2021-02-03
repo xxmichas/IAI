75c7f329d4081dc2f47a62dfc17de7aab3e69b5b
Michal Orkisz <xxmichas@gmail.com>
2021-02-03 05:53:24 +0100

    mobile - part 12

--- src/containers/Mobile/TopMenu/TopMenu.js
+++ src/containers/Mobile/TopMenu/TopMenu.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Basket from '../../TopBar/Basket/Basket'
 import styles from './TopMenu.module.css'
 
 const TopMenu = (props) => {
     return (
-        <div className={styles.TopMenu}>
-            <div onClick={() => props.SetOffCanvasOpened(true)} className={styles.bars} />
-            <div className={styles.logo} />
-            <Basket cart={props.cart} />
-        </div>
+        <Fragment>
+            <div className={styles.space} />
+            <div className={styles.TopMenu}>
+                <div onClick={() => props.SetOffCanvasOpened(true)} className={styles.bars} />
+                <div className={styles.logo} />
+                <Basket cart={props.cart} />
+            </div>
+        </Fragment>
     )
 }
 
