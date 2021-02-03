087dd6b4bdec7d95c333da9890a54fe97a76d0ff
Michal Orkisz <xxmichas@gmail.com>
2021-02-03 19:57:11 +0100

    mobile - part 14

--- src/containers/Mobile/Dropdown/Dropdown.js
+++ src/containers/Mobile/Dropdown/Dropdown.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Dropdown.module.css'
 
 const Dropdown = (props) => {
+    let [dropdownOpen, SetdropdownOpen] = useState(false)
+
     return (
-        <div>
-            
+        <div className={`${styles.dropdown} ${dropdownOpen ? styles.dropdownOpen : null}`}>
+            <div onClick={() => {props.isDropdown ? SetdropdownOpen(prev => !prev) : window.location.assign(props.link)}} className={styles.dropdownHeader}>
+                <div className={styles.dropdownHeaderContainer}>
+                    <div className={styles.dropdownTitle}>{props.title}</div>
+                    {props.isDropdown ? <div className={`${styles.dropdownArrow} ${dropdownOpen ? styles.dropdownArrowRotate : null}`} /> : null}
+                </div>
+            </div>
+            {props.isDropdown ?
+                <div className={styles.expandable}>
+                    {props.options.map((el, i) => (
+                        <div key={i} onClick={() => window.location.assign(el.link)} className={styles.dropdownSection}>{el.name}</div>
+                    ))}
+                </div>
+            : null}
         </div>
     )
 }
--- src/containers/Mobile/OffCanvas/OffCanvas.js
+++ src/containers/Mobile/OffCanvas/OffCanvas.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styles from './OffCanvas.module.css'
 import LanguageSelect from '../../LanguageSelect/LanguageSelect'
+import Dropdown from '../Dropdown/Dropdown'
 
 const OffCanvas = (props) => {
 
-    let [AccountOpen, SetAccountOpen] = useState(false)
+    let accountOptions = [
+        {name: "Zarejestruj się", link: "https://google.com"},
+        {name: "Moje zamówienia", link: "https://google.com"},
+        {name: "Koszyk", link: "https://google.com"},
+        {name: "Ulubione", link: "https://google.com"},
+        {name: "Historia transakcji", link: "https://google.com"}
+    ]
+
+    let bundleOptions = [
+        {name: "zestaw", link: "https://google.com"},
+        {name: "lorem", link: "https://google.com"},
+        {name: "ipsum", link: "https://google.com"},
+    ]
 
     return (
         <div onClick={() => props.SetOffCanvasOpened(false)} className={`${styles.backDrop} ${props.opened ? styles.visible : null}`}>
@@ -16,60 +29,12 @@ const OffCanvas = (props) => {
                     <div className={styles.language}>
                         <LanguageSelect />
                     </div>
-                    <div className={`${styles.account} ${AccountOpen ? styles.accountOpen : null}`}>
-                        <div onClick={() => SetAccountOpen(prev => !prev)} className={styles.accountHeader}>
-                            <div className={styles.accountHeaderContainer}>
-                                <div className={styles.accountTitle}>
-                                    MOJE KONTO
-                                </div>
-                                <div className={`${styles.accountArrow} ${AccountOpen ? styles.accountArrowRotate : null}`} />
-                            </div>
-                        </div>
-                        <div className={styles.expandable}>
-                            <div className={styles.accountSection}>
-                                Zarejestruj się
-                            </div>
-                            <div className={styles.accountSection}>
-                                Moje zamówienia
-                            </div>
-                            <div className={styles.accountSection}>
-                                Koszyk
-                            </div>
-                            <div className={styles.accountSection}>
-                                Ulubione
-                            </div>
-                            <div className={styles.accountSection}>
-                                Historia transakcji
-                            </div>
-                        </div>
-                    </div>
-                    <div className={`${styles.account} ${AccountOpen ? styles.accountOpen : null}`}>
-                        <div onClick={() => SetAccountOpen(prev => !prev)} className={styles.accountHeader}>
-                            <div className={styles.accountHeaderContainer}>
-                                <div className={styles.accountTitle}>
-                                    MOJE KONTO
-                                </div>
-                                <div className={`${styles.accountArrow} ${AccountOpen ? styles.accountArrowRotate : null}`} />
-                            </div>
-                        </div>
-                        <div className={styles.expandable}>
-                            <div className={styles.accountSection}>
-                                Zarejestruj się
-                            </div>
-                            <div className={styles.accountSection}>
-                                Moje zamówienia
-                            </div>
-                            <div className={styles.accountSection}>
-                                Koszyk
-                            </div>
-                            <div className={styles.accountSection}>
-                                Ulubione
-                            </div>
-                            <div className={styles.accountSection}>
-                                Historia transakcji
-                            </div>
-                        </div>
-                    </div>
+                    <Dropdown isDropdown={true} title="MOJE KONTO" options={accountOptions} />
+                    <Dropdown isDropdown={true} title="GOTOWE ZESTAWY DO GRY" options={bundleOptions} />
+                    <Dropdown isDropdown={true} title="SPRZĘT" options={[{name: "sprzęt", link: "https://google.com"}, ...bundleOptions]} />
+                    <Dropdown isDropdown={true} title="ODZIEŻ" options={[{name: "odzież", link: "https://google.com"}, {name: "ubrania", link: "https://google.com"}]} />
+                    <Dropdown isDropdown={false} title="KULKI" link="https://allegro.pl" />
+                    <Dropdown isDropdown={false} title="WYPRZEDAŻE" link="https://allegro.pl" />
                 </div>
             </div>
         </div>
